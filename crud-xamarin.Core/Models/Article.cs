b2f2e84e7afc74b96a1540786f91bd9905f9b0d4
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-08-18 18:56:15 -0300

    backend: add crud-xamarin.Core project

---
+++ crud-xamarin.Core/Models/Article.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace crud_xamarin.Core.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ crud-xamarin.Core/Services/ArticleService.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using crud_xamarin.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace crud_xamarin.Core.Services
+{
+    public class ArticleService
+    {
+        private static List<Article> articles = new List<Article>()
+        {
+            new Article{Id=1, Name="a1"},
+            new Article{Id=2, Name="a2"},
+            new Article{Id=3, Name="a3"}
+        };
+
+        public ArticleService()
+        {
+        }
+
+        public List<Article> GetArticles()
+        {
+            return articles;
+        }
+
+        public Article GetArticleById(int id)
+        {
+            return articles.Where(a => a.Id == id).FirstOrDefault();
+        }
+    }
+}
