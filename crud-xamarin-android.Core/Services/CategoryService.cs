65549d84701c7c246a93eb84d3a3a9b2aaf491ad
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-08-23 23:00:15 -0300

    backend: add CategoryService

---
+++ crud-xamarin-android.Core/Services/CategoryService.cs
@@ -0,0 +1,61 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using crud_xamarin_android.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace crud_xamarin_android.Core.Services
+{
+    class CategoryService
+    {
+        static List<Category> categories = new List<Category>
+        {
+            new Category { Id = 1, Name = "Otro" },
+            new Category { Id = 2, Name = "Pantalones" },
+            new Category { Id = 3, Name = "Remeras" },
+            new Category { Id = 4, Name = "Camisas" },
+            new Category { Id = 5, Name = "Busos" },
+            new Category { Id = 6, Name = "Gorras" },
+        };
+
+        public CategoryService()
+        {
+        }
+
+        public List<Category> GetCategories()
+        {
+            return categories;
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            return categories.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void AddCategory(Category category)
+        {
+            categories.Add(category);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            var categoryToEdit = categories.FirstOrDefault(c => c.Id == category.Id);
+            if (categoryToEdit!=null)
+            {
+                categoryToEdit.Name = category.Name;
+            }
+        }
+
+        public void DeleteCategory(int id)
+        {
+            var category = categories.FirstOrDefault(c => c.Id == id);
+            categories.Remove(category);
+        }
+    }
+}
